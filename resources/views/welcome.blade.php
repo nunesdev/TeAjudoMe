8b2da9e3083c6848710f61058d67b9522d07c78e
bruno.reis <bruno.reis@empiricus.com.br>
2020-04-09 11:57:44 -0300

    debug / apple icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,21 +24,21 @@
         <meta name="msapplication-starturl" content="/">
 
         <link rel="icon" type="image/png" sizes="72x72" href="images/icons/icon-72x72.png">
-        <link rel="apple-touch-icon" type="image/png" sizes="72x72" href="images/icons/icon-72x72.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="72x72" href="images/icons/apple/icon-72x72.png">
         <link rel="icon" type="image/png" sizes="96x96" href="images/icons/icon-96x96.png">
-        <link rel="apple-touch-icon" type="image/png" sizes="96x96" href="images/icons/icon-96x96.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="96x96" href="images/icons/apple/icon-96x96.png">
         <link rel="icon" type="image/png" sizes="128x128" href="mages/icons/icon-128x128.png">
-        <link rel="apple-touch-icon" type="image/png" sizes="128x128" href="mages/icons/icon-128x128.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="128x128" href="mages/icons/apple/icon-128x128.png">
         <link rel="icon" type="image/png" sizes="144x144" href="images/icons/icon-144x144.png">
-        <link rel="apple-touch-icon" type="image/png" sizes="144x144" href="images/icons/icon-144x144.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="144x144" href="images/icons/apple/icon-144x144.png">
         <link rel="icon" type="image/png" sizes="152x152" href="images/icons/icon-152x152.png">
-        <link rel="apple-touch-icon" type="image/png" sizes="152x152" href="images/icons/icon-152x152.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="152x152" href="images/icons/apple/icon-152x152.png">
         <link rel="icon" type="image/png" sizes="192x192" href="images/icons/icon-192x192.png">
-        <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="images/icons/icon-192x192.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="images/icons/apple/icon-192x192.png">
         <link rel="icon" type="image/png" sizes="384x384" href="images/icons/icon-384x384.png">
-        <link rel="apple-touch-icon" type="image/png" sizes="384x384" href="images/icons/icon-384x384.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="384x384" href="images/icons/apple/icon-384x384.png">
         <link rel="icon" type="image/png" sizes="512x512" href="images/icons/icon-512x512.png">
-        <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="images/icons/icon-512x512.png">
+        <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="images/icons/apple/icon-512x512.png">
 
         <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no, shrink-to-fit=no">
         <title>Te Ajudo - Me</title>
@@ -66,6 +66,25 @@
       // appId: "157b4f4f-3001-4ad3-93f8-8bdf9970e51c",
     });
   });
+  window.addEventListener('load', () => {
+  if (navigator.standalone) {
+    console.log('Launched: Installed (iOS)');
+  } else if (matchMedia('(display-mode: standalone)').matches) {
+    console.log('Launched: Installed');
+  } else {
+    console.log('Launched: Browser Tab');
+  }
+});
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  console.log('INstall App');
+});
 </script>
     </head>
     <body >
