807e741c31341462fae065167108ba6d21e1f3ac
bruno.reis <bruno.reis@empiricus.com.br>
2020-04-09 17:25:17 -0300

    add splashscreen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
         <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="images/icons/apple/icon-512x512.png">
 
         <link  rel="apple-touch-startup-image" href="/images/icons/splash/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" />
-        <link  rel="apple-touch-startup-image" href="/images/icons/splash/iphone6_splash" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" />
+        <link  rel="apple-touch-startup-image" href="/images/icons/splash/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" />
         <link  rel="apple-touch-startup-image" href="/images/icons/splash/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" />
         <link  rel="apple-touch-startup-image" href="/images/icons/splash/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" />
         <link  rel="apple-touch-startup-image" href="/images/icons/splash/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" />
