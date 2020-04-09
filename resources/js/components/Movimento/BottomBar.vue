3fbba3e41c2156aedd610209f237a9a021073e08
bruno.reis <bruno.reis@empiricus.com.br>
2020-04-09 12:08:05 -0300

    debug inapp

--- resources/js/components/Movimento/BottomBar.vue
+++ resources/js/components/Movimento/BottomBar.vue
@@ -102,6 +102,7 @@ export default {
       isActiveSidebarMember: false,
       showHandUp: true,
       showMapUp: true,
+      installAppEvent: undefined
     }
   },
   watch:{
@@ -115,6 +116,23 @@ export default {
       'getMarkersMovimento',
       'getTotalMarkersMovimento'
     ]),
+  },
+  created() {
+    window.addEventListener('load', () => {
+      if (navigator.standalone) {
+        console.log('Launched: Installed (iOS)');
+      } else if (matchMedia('(display-mode: standalone)').matches) {
+        console.log('Launched: Installed');
+      } else {
+        console.log('Launched: Browser Tab');
+      }
+    });
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+      e.preventDefault();
+      this.installAppEvent = event;
+      console.log('Can install App',event);
+    });
   },
   mounted() {
     this.showHandUp = this.$router.currentRoute.name == 'Movimento117' ? true : false
@@ -141,36 +159,17 @@ export default {
       this.$emit('sidebarOpen', v);
     },
     installApp() {
-      window.addEventListener('load', () => {
-      if (navigator.standalone) {
-        console.log('Launched: Installed (iOS)');
-      } else if (matchMedia('(display-mode: standalone)').matches) {
-        console.log('Launched: Installed');
-      } else {
-        console.log('Launched: Browser Tab');
-      }
-    });
-    let installPromptEvent;
 
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent the mini-infobar from appearing on mobile
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      installPromptEvent = e;
-      // Update UI notify the user they can install the PWA
-      console.log('INstall App');
-    });
-      // Show the modal add to home screen dialog
-      installPromptEvent.prompt();
+      this.installAppEvent.prompt();
       // Wait for the user to respond to the prompt
-      installPromptEvent.userChoice.then((choice) => {
+      this.installAppEvent.userChoice.then((choice) => {
         if (choice.outcome === 'accepted') {
           console.log('User accepted the A2HS prompt');
         } else {
           console.log('User dismissed the A2HS prompt');
         }
         // Clear the saved prompt since it can't be used again
-        installPromptEvent = null;
+        this.installAppEvent = null;
       });
     }
   }
