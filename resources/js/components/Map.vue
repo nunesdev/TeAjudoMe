19fd4845656f86fc40b1532d2f3b493a6b7125f1
bruno.reis <bruno.reis@empiricus.com.br>
2020-04-11 17:34:20 -0300

    fix css map

--- resources/js/components/Map.vue
+++ resources/js/components/Map.vue
@@ -167,9 +167,11 @@ export default {
 }
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
   .map-inner
     position: absolute
     width: 100%
     height: 100%
+  .location
+    top: 160px
 </style>
