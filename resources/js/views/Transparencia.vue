b7a512e92fbb6df7022b5d69a08d633be145087b
bruno.reis <bruno.reis@empiricus.com.br>
2020-04-16 12:37:31 -0300

    update - transparencia

--- resources/js/views/Transparencia.vue
+++ resources/js/views/Transparencia.vue
@@ -48,9 +48,9 @@ export default {
   data() {
     return {
       isMobile: isMobile,
-      lastUpdated: '16/04/2020 ás 09:38',
+      lastUpdated: '16/04/2020 ás 12:35',
       target: 60,
-      price: 400,
+      price: 450,
       cestas: 0,
       price_food: 60,
       value: 0,
